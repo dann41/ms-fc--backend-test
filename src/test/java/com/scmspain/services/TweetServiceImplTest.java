d3e96ad59403bce153ba170c8b2a14e16105aef9
Dani <daniels26@gmail.com>
2018-05-27 23:45:56 +0200

    Add metrics tests

--- src/test/java/com/scmspain/services/TweetServiceImplTest.java
+++ src/test/java/com/scmspain/services/TweetServiceImplTest.java
@@ -7,7 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 
+import java.util.Collections;
+
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -21,7 +24,7 @@ public class TweetServiceImplTest {
     private TweetService tweetService;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         builder = mock(TweetBuilder.class);
         repository = mock(TweetRepository.class);
         metricWriter = mock(MetricWriter.class);
@@ -29,7 +32,7 @@ public class TweetServiceImplTest {
     }
 
     @Test
-    public void shouldInsertANewTweet() throws Exception {
+    public void shouldInsertANewTweet() {
         // given
         String publisher = "Guybrush Threepwood";
         String text = "I am Guybrush Threepwood, mighty pirate.";
@@ -40,10 +43,11 @@ public class TweetServiceImplTest {
 
         // then
         verify(repository).save(any(Tweet.class));
+        verify(metricWriter).increment(any());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowAnExceptionWhenTweetLengthIsInvalid() throws Exception {
+    public void shouldThrowAnExceptionWhenTweetLengthIsInvalid() {
         // given
         String publisher = "Pirate";
         String text = "LeChuck? He's the guy that went to the Governor's for dinner and never wanted to leave. He fell for her in a big way, but she told him to drop dead. So he did. Then things really got ugly.";
@@ -54,7 +58,7 @@ public class TweetServiceImplTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowAnExceptionWhenPublisherIsEmpty() throws Exception {
+    public void shouldThrowAnExceptionWhenPublisherIsEmpty() {
         // given
         String publisher = "";
         String text = "LeChuck? He's the guy that went to the Governor's for dinner and never wanted to leave. He fell for her in a big way, but she told him to drop dead. So he did. Then things really got ugly.";
@@ -62,6 +66,43 @@ public class TweetServiceImplTest {
 
         // when
         tweetService.publishTweet(publisher, text);
+    }
+
+    @Test
+    public void shouldIncrementMetricWhenListingAllTweets() {
+        // given
+        when(repository.findAllSortedByIdDesc()).thenReturn(Collections.emptyList());
+
+        // when
+        tweetService.listAllTweets();
+
+        // then
+        verify(metricWriter).increment(any());
+    }
+
+    @Test
+    public void shouldIncrementMetricWhenDiscardTweet() {
+        // given
+        Long tweetId = 1L;
+        when(repository.findOne(anyLong())).thenReturn(getTweet("author1", "this is a tweet"));
+
+        // when
+        tweetService.discardTweet(tweetId);
+
+        // then
+        verify(metricWriter).increment(any());
+    }
+
+    @Test
+    public void shouldIncrementMetricWhenListingDiscardedTweets() {
+        // given
+        when(repository.findAllDiscardedSortedByIdDesc()).thenReturn(Collections.emptyList());
+
+        // when
+        tweetService.listDiscardedTweets();
+
+        // then
+        verify(metricWriter).increment(any());
     }
 
     private Tweet getTweet(String publisher, String text) {
